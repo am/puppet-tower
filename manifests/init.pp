68a6a6cbc8f5d2ff995c28c6699c9c26ffdefa13
antonio miranda <antsmiranda@gmail.com>
2013-03-31 17:13:11 +0200

    Add flavour to resource.

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,5 +7,6 @@ class tower {
   package { 'Tower':
     provider => 'compressed_app',
     source   => 'https://macapps.fournova.com/tower1-1060/1.4.19/download',
-  }
+    flavor   => 'zip',
+}
 }
--- spec/classes/tower_spec.rb
+++ spec/classes/tower_spec.rb
@@ -5,6 +5,7 @@ describe 'tower' do
     should contain_package('Tower').with({
       :provider => 'compressed_app',
       :source   => 'https://macapps.fournova.com/tower1-1060/1.4.19/download',
+      :flavor   => 'zip',
     })
   end
 end
